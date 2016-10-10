a2f3a8e9a6e984e4eb42be5fd12e166ef20b70e3
Vlad Golubev <golubev.vld@gmail.com>
2016-10-10 23:19:39 +0300

    Initial commit

---
+++ lab-01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task1.Run();
+        }
+    }
+}
---
+++ lab-01/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("lab_01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("$safeprojectname$")]
+[assembly: AssemblyCopyright("Copyright ©  116")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("6051A192-04FF-4536-8BAA-A48AE414CDBE")]
+
