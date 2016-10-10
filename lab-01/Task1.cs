73ca626bd69d216119ab39063218c2d914908877
Vlad Golubev <golubev.vld@gmail.com>
2016-10-10 23:19:51 +0300

    Add first task solution

---
+++ lab-01/Task1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab_01
+{
+    public class Task1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Please enter your first, last and middle name"
+                              + " (separated by space) and press dot at the end.");
+
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"Hello \n {input.Replace(".", "")}");
+        }
+    }
+}
