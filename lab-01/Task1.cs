f81740db82bed4d19c12e2d8bf57d47eb113cd6e
Vlad Golubev <golubev.vld@gmail.com>
2016-10-18 13:46:54 +0300

    Fix first task after review

--- lab-01/Task1.cs
+++ lab-01/Task1.cs
@@ -9,9 +9,23 @@ namespace lab_01
             Console.WriteLine("Please enter your first, last and middle name"
                               + " (separated by space) and press dot at the end.");
 
-            var input = Console.ReadLine();
-
+            var input = GetInputStringUntilDot();
+            Console.Clear();
             Console.WriteLine($"Hello \n {input.Replace(".", "")}");
+        }
+
+        private static string GetInputStringUntilDot()
+        {
+            var input = "";
+            ConsoleKeyInfo key;
+
+            while (key.KeyChar != '.')
+            {
+                key = Console.ReadKey();
+                input += key.KeyChar;
+            }
+
+            return input;
         }
     }
 }
